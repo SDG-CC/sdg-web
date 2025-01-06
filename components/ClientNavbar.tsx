6b626c9161aa56f002d7eb845dd987425c68227b
Shyamsunder Tard <iamshyamsundertard@gmail.com>
2025-01-06 20:35:22 +0530

    Adding login-logout option along with user details to Navbar

--- components/ClientNavbar.tsx
+++ components/ClientNavbar.tsx
@@ -1,14 +1,35 @@
 "use client";
 
 import { motion } from "framer-motion";
+import { signIn, signOut } from "next-auth/react";
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import Image from "next/image";
+import { Button } from "./ui/button";
+import { useSearchParams } from "next/navigation";
+import {
+    User,
+    LogOut
+} from "lucide-react"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
+  
+interface ClientNavbarProps {
+  session: any;
+}
 
-
-const ClientNavbar= () => {
+const ClientNavbar: React.FC<ClientNavbarProps> = ({ session }) => {
   const [isVisible, setIsVisible] = useState<boolean>(true);
   const [lastScrollY, setLastScrollY] = useState<number>(0);
+
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl") || "/"
 
   const handleScroll = () => {
     if (typeof window !== "undefined") {
@@ -39,6 +60,8 @@ const ClientNavbar= () => {
     visible: { y: 0, opacity: 1, transition: { duration: 0.8 } },
   };
 
+  const firstLetter = session?.user.name.charAt(0).toUpperCase()
+
   return (
     <motion.nav
       className="w-full h-16 shadow-lg rounded-b-2xl px-2 flex items-center bg-gradient-to-r from-blue-500 via-sky-600 to-emerald-500"
@@ -63,6 +86,38 @@ const ClientNavbar= () => {
           <li className="mx-5">
             <Link href="/contact">Contact</Link>
           </li>
+          {session ? (
+            <li className="mx-5">
+                <DropdownMenu>
+                <DropdownMenuTrigger 
+                className="bg-blue-600 rounded-full p-1 w-8 hover:scale-125 active:scale-75 transform-transform duration-75 focus:outline-dashed outline-dashed outline-blue-600">
+                    {firstLetter}
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuLabel>{session.user.name}</DropdownMenuLabel>
+                    <DropdownMenuSeparator className="bg-gray-300"/>
+                    <DropdownMenuItem>
+                        <User/>
+                        <span>Profile</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator className="bg-gray-100 m-0"/>
+                    <DropdownMenuItem onClick={() => signOut()}>
+                        <LogOut/>
+                        <span>Logout</span>
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+                </DropdownMenu>
+            </li>
+          ) : (
+            <li className="mx-5">
+              <Button
+              variant="login"
+                onClick={() => signIn()}
+              >
+                Login
+              </Button>
+            </li>
+          )}
         </ul>
       </div>
     </motion.nav>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,12 @@
 import ClientNavbar from "./ClientNavbar";
+import { auth } from "@/lib/auth";
 
 export default async function Navbar() {
+  const session = await auth()
 
   return (
     <div className="fixed top-0 left-0 right-0 z-50 w-full">
-      <ClientNavbar />
+      <ClientNavbar session={session} />
     </div>
   );
 }
